e4998c235bb7fe9d77f01a2ac6561f0e108ca876
Nitin Prajapati <78595923+Nitinprajapati393@users.noreply.github.com>
2024-08-01 18:07:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
+  client_id       = "19d217e2-330e-4bef-a636-c0a9e1784a45"
+  client_secret   = "tr08Q~mn9QuvMuMabn7VJtlHTj4xHA9SXAg3saYH"
+  subscription_id = "e19adf60-9df9-4cc0-b7fe-b57eb6ae96de"
+  tenant_id       = "47c2a87f-6eee-466d-954e-e21ffadb7d86"
   features {}
 }
 
